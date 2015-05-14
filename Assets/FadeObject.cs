055bbd9bba0e6e8680ba033911d2a1e23310ba5b
tinykidtoo <tinykidtoodotcom@yahoo.com>
2015-05-14 12:46:03 -0700

    FirstCommit may/14/15 12:45am
    the first one with basic movement and fading tech

---
+++ Assets/FadeObject.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeObject : MonoBehaviour
+{
+	Renderer myRender;
+	public GameObject myObject;
+	public float fadeTime = 0.05f;
+	float aCurrent = 1f;
+	float tar = 1f;
+	Color originalColour;
+	bool playerBehind = false;
+	public bool canStealthBehind = true;
+
+	// Use this for initialization
+	void Start ()
+	{
+		myRender = myObject.GetComponent<Renderer> ();
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.tag == "Player") {
+			if (canStealthBehind) {
+				PlayerMoveTEst tempMove = col.gameObject.GetComponent<PlayerMoveTEst> ();
+				tempMove.behindStealthableObj = true;
+			}
+			playerBehind = true;
+		}
+	}
+
+	void OnTriggerExit (Collider col)
+	{
+		if (col.gameObject.tag == "Player") {
+			if (canStealthBehind) {
+				PlayerMoveTEst tempMove = col.gameObject.GetComponent<PlayerMoveTEst> ();
+				tempMove.behindStealthableObj = false;
+			}
+			playerBehind = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (playerBehind) 
+			tar = 0.1f;
+		else
+			tar = 1f;
+		aCurrent = Mathf.Lerp (aCurrent, tar, fadeTime);
+		originalColour = myRender.material.color;
+		myRender.material.color = new Color (originalColour.r, originalColour.g, originalColour.b, aCurrent);
+	}
+}
---
+++ Assets/PlayerMoveTEst.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMoveTEst : MonoBehaviour {
+	public NavMeshAgent agent;
+	public bool behindStealthableObj = false;		//I am Behind something
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)){
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		
+			if (Physics.Raycast (ray, out hit)) {
+				Transform objectHit = hit.transform;
+				Debug.DrawLine (Camera.main.transform.position, hit.point, Color.red);
+				NavMeshHit navHit;
+				NavMesh.SamplePosition(hit.point, out navHit, 20f, NavMesh.AllAreas);
+				agent.SetDestination(navHit.position);
+			}
+		}
+	}
+}
